fc8b5349d9e36d6148064c5aeb3d8de5d0a7fca1
White8912 <white.pro8912@gmail.com>
2022-12-20 14:53:25 -0500

    update our menu-items to navigate useNavigate

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import HomePage from "./pages/HomePage/HomePage.component.jsx";
 
+import { BrowserRouter as Router,Routes, Route } from "react-router-dom";
+
 import "./App.css";
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
-    <div>
-      <HomePage />
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route path="/hats" element={<HatsPage />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/MenuItem/MenuItem.component.jsx
+++ src/components/MenuItem/MenuItem.component.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 
+import {
+    useLocation,
+    useNavigate,
+    useParams,
+    useRouteMatch
+} from "react-router-dom";
+
 import './menuitem.styles.scss'
 
-const MenuItem = ({ title, imageUrl, size }) => {
+
+const MenuItem = ({size, imageUrl, title, linkUrl}) => {
+    // let location = useLocation();
+    let navigate = useNavigate();
+    let params = useParams();
+    // console.log("menuitem withrouter: ", userId)
     return (
-        <div className={`menu-item ${size}`}>
+        <div className={`menu-item ${size}`} onClick={() => navigate(`${linkUrl}`)}>
             <div className='background-image' style={{
                 backgroundImage: `url(${imageUrl})`
             }} />
@@ -16,4 +28,4 @@ const MenuItem = ({ title, imageUrl, size }) => {
     )
 }
 
-export default MenuItem
+export default MenuItem;
